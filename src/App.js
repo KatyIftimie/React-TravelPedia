b8e7992e25a7b340897d7c7894b2cf2e533dbe1e
AlexCatalin21 <alexandru.g2101@gmail.com>
2020-08-25 11:43:07 +0300

    Continent cards implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarLayout from './components/layout/Navbar'
+import Home from './components/layout/Home'
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App" >
+        <NavbarLayout/>
+        <Home/>
     </div>
   );
 }
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navbar, Nav, Form, Button, FormControl } from 'react-bootstrap'
+import logo from '../../img/logo.png';
+
+export default function NavbarLayout() {
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home"><img style={logoStyle} src={logo} alt="logo" ></img></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Visited</Nav.Link>
+            <Nav.Link href="#link">Wish List</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+}
+
+
+const logoStyle={
+    width:"45%",
+    height:"45%"
+};
