84d62b1b7917b75a2b3925f659662a61a7b7ed2a
alecsanderu <oanceaalexander@yahoo.com>
2020-08-25 15:27:31 +0300

    routes for countries working

--- src/components/continents/Europe.js
+++ src/components/continents/Europe.js
@@ -1,31 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Country from "../countries/Country"
-import { Link } from 'react-router-dom'
+import Country from "../countries/Country";
+import { Link } from "react-router-dom";
 
 export default function Europe() {
-  const [countries, setCountries] = useState([])
+  const [countries, setCountries] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const res = await axios.get('https://restcountries.eu/rest/v2/region/europe')
+      const res = await axios.get(
+        "https://restcountries.eu/rest/v2/region/europe"
+      );
 
       setCountries(res.data);
-      console.log(res.data)
+      console.log(res.data);
     }
-    fetchData()
-
-  }, [setCountries])
+    fetchData();
+  }, [setCountries]);
 
   return (
     <div className="container">
       <div className="row">
         {countries.map((country, i) => (
           <>
-            <Link to={`/details/${country.name}`} >
-              <Country key={i} country={country} />
-            </Link>
-
+            <Country key={i} country={country} />
           </>
         ))}
       </div>
--- src/components/countries/CountryDetails.js
+++ src/components/countries/CountryDetails.js
@@ -1,23 +1,26 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function CountryDetails(props) {
-    const [details, setDetails] = useState([])
-    // console.log("Details", props.match.params.name)
-    // console.log(props)
-    const name = props.match.params.name
-    useEffect(() => {
-        async function fetchData() {
-            const res = await axios.get('https://restcountries.eu/rest/v2/name/' + { name })
+  const [details, setDetails] = useState([]);
+  console.log(props);
+  const name = props.location.pathname.split("/")[2];
+  console.log(name);
 
-            setDetails(res.data)
-        }
-        fetchData()
-    }, [])
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios.get(
+        `https://restcountries.eu/rest/v2/name/${name}`
+      );
+      console.log(res.data[0].name);
+      setDetails(res.data[0]);
+    }
+    fetchData();
+  }, [name, props]);
 
-    return (
-        <div>
-            <p>{details.name}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>{details.name}</p>
+    </div>
+  );
 }
