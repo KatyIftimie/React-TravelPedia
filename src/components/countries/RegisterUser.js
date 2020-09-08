42f70169b9eedd3b88d359d3fd540110be462c09
KatyIftimie <ecaterina.iftimie@gmail.com>
2020-09-08 12:01:26 +0300

    Completed registration form

--- src/components/countries/RegisterUser.js
+++ src/components/countries/RegisterUser.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
+import "../../style/RegisterForm.css";
 import { addUser } from "../service/AddUserToDb";
 
 export default function RegisterUser() {
@@ -11,6 +12,7 @@ export default function RegisterUser() {
   const onSubmit = (value) => {
     let user = value;
     addUser(user);
+    console.log(value);
   };
 
   return (
@@ -25,8 +27,61 @@ export default function RegisterUser() {
         placeholder="Email"
       />
       {errors.email && errors.email.type === "required" && (
-        <div className="error">Your must enter your email address.</div>
+        <p>Your must enter your email address.</p>
       )}
+      <label>First Name: </label>
+      <input
+        type="text"
+        name="firstName"
+        ref={register({
+          required: true,
+        })}
+        placeholder="First Name"
+      />
+      {errors.firstName && errors.firstName.type === "required" && (
+        <p>Your must enter your first name.</p>
+      )}
+      <label>Last Name: </label>
+      <input
+        type="text"
+        name="lastName"
+        ref={register({
+          required: true,
+        })}
+        placeholder="Last Name"
+      />
+      {errors.lastName && errors.lastName.type === "required" && (
+        <p>Your must enter your last name.</p>
+      )}
+
+
+      <label>Password: </label>
+      <input
+        type="text"
+        name="password"
+        placeholder="Your password"
+        ref={register({
+          required: "You must specify a password",
+          minLength: {
+            value: 8,
+            message: "Password must have at least 8 characters"
+          }
+        })}
+      />
+      {errors.password && <p>{errors.password.message}</p>}
+
+      <label>Confirm password</label>
+      <input
+        name="confirmPassword"
+        type="text"
+        placeholder="Confirm password"
+        ref={register({
+          validate: value =>
+            value === password.current || "The passwords do not match"
+        })}
+      />
+      {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+
 
       <input type="submit" onClick={handleSubmit(onSubmit)} />
     </form>
