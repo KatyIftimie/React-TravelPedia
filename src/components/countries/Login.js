93573808a9dbc08301b202a2ebb614f69130cb69
alecsanderu <oanceaalexander@yahoo.com>
2020-09-09 09:53:13 +0300

    login enhance

--- src/components/countries/Login.js
+++ src/components/countries/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import "../../style/RegisterForm.css";
 import axios from "axios";
@@ -7,7 +8,10 @@ export default function Login() {
   const { register, handleSubmit } = useForm({});
   const [logInMsj, setLogInMsj] = useState("");
   const [isLoggedIn, setIsLogged] = useState(false);
+
   const LOGIN_API = "http://localhost:8080/api/v1/auth/login";
+
+  const history = useHistory();
 
   const onSubmit = (user) => {
     axios
@@ -16,6 +20,11 @@ export default function Login() {
         if (res.status === 200) {
           setIsLogged(true);
           setLogInMsj(res.data);
+          console.log(res);
+          // setTimeout(() => {
+          //   history.push("/");
+          // }, 500);
+
           axios
             .get("http://localhost:8080/api/v1/auth/is-logged-in")
             .then((res) => console.log(res));
