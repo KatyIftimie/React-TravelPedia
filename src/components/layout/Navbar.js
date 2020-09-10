d096520cd9b27c0854bd250e0ce28bf2bcce8efd
alecsanderu <oanceaalexander@yahoo.com>
2020-09-10 16:22:46 +0300

    navBar changing on login

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Navbar, Nav, Form, Button, FormControl } from "react-bootstrap";
+import { useCallback } from "react";
 import logo from "../../img/logo.png";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -9,24 +10,26 @@ export default function NavbarLayout() {
   const [query, setQuery] = useState("");
   const [userIsLogin, setIsLogin] = useState("");
   const [userEmail, setUserEmail] = useState("");
+  const [details, setDetails] = useState("");
 
-  const USER_API = "localhost:8080/api/v1/auth/get-user/";
-  const getUserDetails = async () => {
-    await axios.get(USER_API + userEmail).then((res) => {
-      console.log(res);
+  const USER_API = "http://localhost:8080/api/v1/auth/get-user/";
+
+  const getUserDetails = useCallback(() => {
+    axios.get(USER_API + userEmail).then((res) => {
+      console.log(res.data);
+      setDetails(res.data);
     });
-  };
+  });
 
   useEffect(() => {
     if (window.sessionStorage.getItem("login")) {
       setIsLogin(true);
       setUserEmail(window.sessionStorage.getItem("login"));
-
       getUserDetails();
     } else {
       setIsLogin(false);
     }
-  }, [getUserDetails, userEmail, userIsLogin]);
+  }, [getUserDetails]);
 
   const handleClick = () => {
     setQuery(title);
@@ -41,10 +44,19 @@ export default function NavbarLayout() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/login">{userIsLogin ? null : "Login"}</Nav.Link>
-            <Nav.Link href="/logout">{userIsLogin ? `Logout` : null}</Nav.Link>
+            {userIsLogin ? (
+              <Nav.Link href="/logout"> Logout</Nav.Link>
+            ) : (
+              <Nav.Link href="/login"> Login </Nav.Link>
+            )}
             <Nav.Link href="#">
-              Welcome {userIsLogin ? sessionStorage.getItem("login") : null}
+              Welcome {userIsLogin ? details.lastName : null}
+            </Nav.Link>
+            <Nav.Link href="#">
+              {details && details.type.name === "HOST" ? "ADD RENTAL" : null}
+            </Nav.Link>
+            <Nav.Link href="#">
+              {details && details.type.name === "GUEST" ? "See bookings" : null}
             </Nav.Link>
           </Nav>
           <Form inline>
